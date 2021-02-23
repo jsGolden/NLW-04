465c825d67ce8852cc98ad7e246dc7fabe912879
jsGolden <foolycoolyvip@gmail.com>
2021-02-23 13:52:19 -0300

    Adicionado sistema de controles/rotas

--- net-promoter-score/src/server.ts
+++ net-promoter-score/src/server.ts
@@ -1,17 +1,11 @@
 import 'reflect-metadata'
 import express from 'express';
 import  './database';
+import router from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World - NLW04' })
-});
+app.use(express.json())
+app.use(router);
 
-app.post("/", (request, response) => {
-    return response.json({ message: 'Os dados foram salvos com sucesso!' });
-});
-
-app.listen(3333, () => {
-    console.log('Servidor rodando na porta 3333!');
-});
+app.listen(3333, () => console.log('Servidor rodando na porta 3333!'));
