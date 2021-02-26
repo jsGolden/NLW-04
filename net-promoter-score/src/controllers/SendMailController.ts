c987d8fa194f20c25db9bc980b29cb1ac4a5a2da
jsGolden <foolycoolyvip@gmail.com>
2021-02-26 12:32:05 -0300

    Código de envio de email refatorado

--- net-promoter-score/src/controllers/SendMailController.ts
+++ net-promoter-score/src/controllers/SendMailController.ts
@@ -29,19 +29,22 @@ class SendMailController {
         }
 
         const npsPath = resolve(__dirname, '..', 'views', 'emails', 'npsMail.hbs');
+
+        const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
+            where: {user_id: user.id, value: null},
+            relations: ["user", "survey"]
+        });
+
         const variables = {
             name: user.name,
             title: survey.title,
             description: survey.description,
-            user_id: user.id,
+            id: "",
             link: process.env.URL_MAIL
         };
 
-        const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
-            where: [{user_id: user.id}, {value: null}],
-            relations: ["user", "survey"]
-        });
         if(surveyUserAlreadyExists) {
+            variables.id = surveyUserAlreadyExists.id;
             await SendMailService.execute(email, survey.title, variables, npsPath);
             return response.json(surveyUserAlreadyExists);
         }
@@ -53,6 +56,7 @@ class SendMailController {
             survey_id 
         });
         await surveysUsersRepository.save(surveyUser);
+        variables.id = surveyUser.id;
 
         // * Send email to user 
         await SendMailService.execute(email, survey.title, variables, npsPath);
